29dffd0acdd0024192aaf6b9af071db35750a05e
Your Name <tomposki@gmail.com>
2023-01-14 17:50:00 +0100

    converted code to functions

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -13,15 +13,10 @@ def get_input(input:List[str]):
 def get_empty_list():
   return [], []
 
-random_l = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
-
-empty_l, empty_ls =[], []
 
 def split_input(input):
   return [regex.split(r" ", ele) for ele in input]
 
-for ele in random_l:
-  empty_ls.append(regex.split(r" ", ele))
 
 def do_math2(element):
   foo = []
@@ -33,40 +28,31 @@ def do_math2(element):
   return foo
 
 
+def add_sign(split_list:list):
+    for i in range(len(split_list)):
+        split_list[i].append("    ----")
+    return split_list
+
 def merge_operators_results(math_list: list, split_list: list):
     for k, v in enumerate(math_list):
         split_list[k].append(math_list[k][0])
     return split_list
 
 
-def do_math(element):
-  a, b = int(element[0]), int(element[2])
-  if element[1] == '+':
-    empty_l.append(a + b)
-  elif element[1] == '-':
-    empty_l.append(a - b)
-  return empty_l
-
 def apply_math(split_input:List[str]):
   return [do_math2(x) for x in split_input]
 
 
-[do_math(x) for x in empty_ls]
-
-for i in range(len(empty_l)):
-    empty_ls[i].append("    ----")
-    empty_ls[i].append(empty_l[i])
-
-TODO: "print in one line"
-for i in range(5):
-  for key, value in enumerate(empty_ls):
-      print("{0:{align}8}".format(empty_ls[key][i],align='>'), end= " ")
-  print("\n".lstrip())
+def print_results(problems):
+    for i in range(5):
+        for key, value in enumerate(problems):
+            print("{0:{align}8}".format(problems[key][i],align='>'), end= " ")
+        print("\n".lstrip())
 
 
-input = get_input(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
-split = split_input(input)
+problems = get_input(["100 + 100", "3801 - 2", "45 + 43", "123 + 49"])
+split = split_input(problems)
 math = apply_math(split)
-calculation = merge_operators_results(math, split)
-#TODO: join two lists: split, math, into one
-#redundant line to be removed in foreseeable future
+dashes = add_sign(split)
+calculation = merge_operators_results(math, dashes)
+print_results(calculation)
